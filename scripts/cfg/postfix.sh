2bfe7e96318bdf0b9cd0b26332dc8eb4a08c54b3
Dashamir Hoxha <dashohoxha@gmail.com>
2017-12-29 22:52:42 +0100

    Improve cfg/postfix.sh

--- scripts/cfg/postfix.sh
+++ scripts/cfg/postfix.sh
@@ -7,12 +7,15 @@ source /host/settings.sh
 echo "own-mailbox-proxy" > /etc/hostname
 echo "127.0.0.1 own-mailbox-proxy" >> /etc/hosts
 
-cd /root/
-git clone https://github.com/Own-Mailbox/postfix-smpt2tor-relay.git
-cd postfix-smpt2tor-relay/
+dir=/opt/Own-Mailbox/postfix-smpt2tor-relay
+rm -rf $dir
+git clone https://github.com/Own-Mailbox/postfix-smpt2tor-relay.git $dir
+cd $dir
 cp postfix-cfg/* /etc/config/postfix/
 
 # ToDo: Edit /etc/config/transport.mysql and add your login to the database
 
 cp scripts/* /usr/lib/postfix/
+
+sed -i /etc/postfix/master.cf -e '/^smtptor unix - - - - - smtp_tor$/d'
 echo "smtptor unix - - - - - smtp_tor" >> /etc/postfix/master.cf

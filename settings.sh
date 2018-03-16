3e5bb93b2761c19761a7c7f89a7d113837363d5b
Pierre Parent <pierre.parent@pparent.fr>
2018-03-16 16:15:46 +0100

    Reorganise settings

--- settings.sh
+++ settings.sh
@@ -1,20 +1,24 @@
-APP=ombproxy
-IMAGE=ombproxy
-CONTAINER=ombproxy
-PORTS="80:80 443:443 53:53 6565:6565"
-
+#Our public IP (the proxy needs a public IP)
 SERVER_IP=164.132.40.32
 
 #This proxy will give subdomains of MASTER_DOMAIN
+#DND authority for MASTER_DOMAIN must be this machine
 MASTER_DOMAIN=omb.one
 FQDN=proxy.$MASTER_DOMAIN
 EMAIL=contact@omb.example.org
 
+#Database related
 DBNAME=proxy
 DBUSER=proxy
 DBPASS='cutAlfApel1TibMunyoss3'
 
 DEV=true
+
+#Docker related
+APP=ombproxy
+IMAGE=ombproxy
+CONTAINER=ombproxy
+PORTS="80:80 443:443 53:53 6565:6565"
 
 # ### Gmail account for notifications (by ssmtp).
 # ### Make sure to enable less-secure-apps:

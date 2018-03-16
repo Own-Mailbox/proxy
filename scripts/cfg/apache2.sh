dca9a5f64ac4b58d2efd06940560783700590fd3
Pierre Parent <pierre.parent@pparent.fr>
2018-03-16 15:14:05 +0100

    Fix apache2

--- scripts/cfg/apache2.sh
+++ scripts/cfg/apache2.sh
@@ -6,12 +6,14 @@ mkdir -p /var/log/apache2
 
 ### create a configuration file
 mkdir -p /var/www/html
-cp $APP_DIR/src/apache2.conf /etc/apache2/sites-available/default.conf
+cp $APP_DIR/src/site-apache2-default.conf /etc/apache2/sites-available/default.conf
+cp $APP_DIR/src/site-apache2-default.conf /etc/apache2/sites-available/ssl.conf
 sed -i /etc/apache2/sites-available/default.conf \
     -e "s#ServerAdmin.*#ServerAdmin $EMAIL#" \
     -e "s#ServerName.*#ServerName $FQDN#" \
     -e "s#proxy.omb.one#$FQDN#"
 
+    
 ### copy letsencrypt.cgi
 mkdir -p /usr/lib/cgi-bin/
 cp $APP_DIR/src/letsencrypt.cgi /usr/lib/cgi-bin/
@@ -25,4 +27,5 @@ a2enmod ssl
 a2enmod rewrite
 a2ensite default
 a2dissite 000-default
+rm /etc/apache2/sites-available/ 000-default
 service apache2 restart

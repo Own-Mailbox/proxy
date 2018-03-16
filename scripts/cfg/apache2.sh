88b48c5ebec757183209443eade8dd70ea6c8994
Pierre Parent <pierre.parent@pparent.fr>
2018-03-16 15:41:33 +0100

    Further correction for apache2

--- scripts/cfg/apache2.sh
+++ scripts/cfg/apache2.sh
@@ -6,14 +6,14 @@ mkdir -p /var/log/apache2
 
 ### create a configuration file
 mkdir -p /var/www/html
-cp $APP_DIR/src/site-apache2-default.conf /etc/apache2/sites-available/default.conf
-cp $APP_DIR/src/site-apache2-default.conf /etc/apache2/sites-available/ssl.conf
+cp $APP_DIR/src/apache/default.conf /etc/apache2/sites-available/
+cp $APP_DIR/src/apache/ssl.conf /etc/apache2/sites-available/
+cp $APP_DIR/src/apache/ports.conf /etc/apache2/
 sed -i /etc/apache2/sites-available/default.conf \
     -e "s#ServerAdmin.*#ServerAdmin $EMAIL#" \
     -e "s#ServerName.*#ServerName $FQDN#" \
     -e "s#proxy.omb.one#$FQDN#"
 
-    
 ### copy letsencrypt.cgi
 mkdir -p /usr/lib/cgi-bin/
 cp $APP_DIR/src/letsencrypt.cgi /usr/lib/cgi-bin/
@@ -22,10 +22,21 @@ chmod +x /usr/lib/cgi-bin/letsencrypt.cgi
 ### Create ok file
 echo "OK"> /var/www/html/OK
 
+### Allow omb customers to get letsencrypt certificates
+mkdir -p /var/www/html/.well-known/acme-challenge/
+
+cat <<EOF>> > /var/www/html/.well-known/acme-challenge/.htaccess
+        RewriteEngine On
+        RewriteBase /
+        RewriteRule  "^(.*)"  "/cgi-bin/letsencrypt.cgi"   [L]
+        Redirect "/test/gloups.html" "http://proxy.omb.one/"
+EOF
+
 ### enable ssl etc.
 a2enmod ssl
 a2enmod rewrite
 a2ensite default
 a2dissite 000-default
-rm /etc/apache2/sites-available/ 000-default
+rm /etc/apache2/sites-available/000-default.conf 
+
 service apache2 restart

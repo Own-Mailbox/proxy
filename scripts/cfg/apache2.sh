d095a5036cb22b8d99e996245c57e4eeb825ed32
giorgos314 <pipilisg@gmail.com>
2018-08-07 20:46:59 +0300

    Proper apache config in order to enable htaccess

--- scripts/cfg/apache2.sh
+++ scripts/cfg/apache2.sh
@@ -7,17 +7,22 @@ mkdir -p /var/log/apache2
 
 ### create a configuration file
 mkdir -p /var/www/html
+
+rm /etc/apache2/apache2.conf
+cp $APP_DIR/src/apache2/apache2.conf /etc/apache2/
+
 cp $APP_DIR/src/apache2/default.conf /etc/apache2/sites-available/
 cp $APP_DIR/src/apache2/ssl.conf /etc/apache2/sites-available/
 cp $APP_DIR/src/apache2/ports.conf /etc/apache2/
+
 sed -i /etc/apache2/sites-available/default.conf \
     -e "s#ServerAdmin.*#ServerAdmin $EMAIL#" \
     -e "s#ServerName.*#ServerName $FQDN#" \
-    -e "s#proxy.omb.one# $FQDN#"
+    -e "s#proxy.omb.one#$FQDN#"
 
 sed -i /etc/apache2/sites-available/ssl.conf \
     -e "s#ServerName.*#ServerName $FQDN#" \
-    -e "s#proxy.omb.one# $FQDN#"
+    -e "s#proxy.omb.one#$FQDN#"
 
 ### copy letsencrypt.cgi
 mkdir -p /usr/lib/cgi-bin/
@@ -29,7 +34,7 @@ echo "OK"> /var/www/html/OK
 
 ### Allow omb customers to get letsencrypt certificates
 sed -i $APP_DIR/src/apache2/.htaccess \
-    -e "s#proxy.omb.one# $FQDN#"
+    -e "s#proxy.omb.one#$FQDN#"
 
 mkdir -p /var/www/html/.well-known/acme-challenge/
 cp $APP_DIR/src/apache2/.htaccess /var/www/html/.well-known/acme-challenge/

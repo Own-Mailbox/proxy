e3cab730cc9c5db61ac6228b751f26f7e7d3fdb0
giorgos314 <pipilisg@gmail.com>
2018-08-08 21:06:19 +0300

    Proper file permissions for add-dovecot-user

--- scripts/cfg/dovecot.sh
+++ scripts/cfg/dovecot.sh
@@ -7,5 +7,10 @@ source /host/settings.sh
 mv /etc/dovecot/dovecot.conf /etc/dovecot/dovecot.conf.old
 cp $APP_DIR/src/dovecot/dovecot.conf /etc/dovecot/dovecot.conf
 
+# Apply permissions regarding the users file
+touch /etc/dovecot/users
+chmod 664 /etc/dovecot/users
+chmod 755 add-dovecot-user.sh
+
 # Replace omb.one with our custom domain
 sed -i /etc/dovecot/dovecot.conf -e "s#proxy.omb.one#$FQDN#g"
